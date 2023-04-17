e6d75835b17d9f63c4359fe753917dd82b6d3c1e
Michal Findra <miskofindra@gmail.com>
2023-04-17 23:02:40 +0200

    update tshark command and fix -peers to see number of connections

--- bt-monitor.py
+++ bt-monitor.py
@@ -9,44 +9,61 @@ def extract_bootstrap_nodes(csv_file):
     with open(csv_file, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=';')
         for row in csv_reader:                                 
-            if row and 'get_peers' in str(row) and str(row[6]).endswith("y,q") and str(row[6]).startswith("a,bs,id"):
+            if row and 'get_peers' in str(row) and str(row[7]).endswith("y,q") and str(row[7]).startswith("a,bs,id"):
                 bootstrap_nodes.add((row[2], row[3]))
 
     return sorted(list(bootstrap_nodes), key=lambda x: x[1])
 
 def extract_peers(csv_file):
     peers_nodes = set()
+    connections = dict()
 
     with open(csv_file, newline='') as csvfile:
         csv_reader = csv.reader(csvfile, delimiter=';')
         for row in csv_reader:
-            if row and 'nodes' in str(row) and str(row[6]).endswith("y,r"):
-                ip_field = row[4]
-                port_field = row[5]
-                ids_field = row[9]
+            if row and 'nodes' in str(row) and str(row[7]).endswith("y,r"):
+                ip_field = row[5]
+                port_field = row[6]
+                ids_field = row[10]
 
-                nodes_flags = row[7]
-                peers_flags = row[8]
+                nodes_flags = row[8]
+                peers_flags = row[9]
                 n_flags = nodes_flags.split(',')
                 if len(peers_flags) != 0:
                     peers_flags = peers_flags.split(',')
-                
+
                 ips = (ip_field.split(','))
                 ports = (port_field.split(','))
-                
                 ids = (ids_field.split(','))
-
 
                 for i in range(len(n_flags)):
                     if n_flags[i] == '1':
-                        peers_nodes.add((ips[i], int(ports[i]), ids[i]))
+                        key = (ips[i], int(ports[i]), ids[i])
+                        peers_nodes.add(key)
+                        connections[key] = 0
 
                 for i in range(len(peers_flags)):
                     if peers_flags[i] == '1':
-                        peers_nodes.add((ips[i], int(ports[i]), ids[i]))
-                
+                        key = (ips[i], int(ports[i]), ids[i])
+                        peers_nodes.add(key)
+                        connections[key] = 0
 
-    return sorted(list(peers_nodes), key=lambda x: x[1])
+        # Second iteration to count connections
+        csvfile.seek(0)  # Move the file pointer back to the beginning
+        for row in csv_reader:
+            if row and str(row[7]).endswith("y,r"):
+                src_ip = row[1]
+                src_port = int(row[4])
+
+                for key in peers_nodes:
+                    if key[0] == src_ip and key[1] == src_port:
+                        connections[key] += 1
+                        break
+    
+    sorted_peers_nodes = sorted(list(peers_nodes), key=lambda x: x[1])
+    return [(node[0], node[1], node[2], connections[node]) for node in sorted_peers_nodes]
+
+
 
 
 
@@ -54,7 +71,7 @@ def process_pcap(pcap_file, mode):
     with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as temp_csv:
         tshark_command = [
             'tshark', '-r', pcap_file, '-T', 'fields', '-E', 'separator=;', '-d', 'udp.port==47222,bt-dht',
-            '-e', 'frame.time_relative', '-e', 'ip.src', '-e', 'ip.dst', '-e', 'udp.dstport', '-e', 'bt-dht.ip', '-e', 'bt-dht.port',
+            '-e', 'frame.time_relative', '-e', 'ip.src', '-e', 'ip.dst', '-e', 'udp.dstport', '-e', 'udp.srcport', '-e', 'bt-dht.ip', '-e', 'bt-dht.port',
             '-e', 'bt-dht.bencoded.string', '-e', 'bt-dht.node', '-e', 'bt-dht.peer', '-e', 'bt-dht.id',
             '-e', 'bittorrent.piece.index', '-e', 'bittorrent.piece.begin', '-e', 'bittorrent.piece.length',
             '-e', 'bittorrent.peer_id', '-e', 'bittorrent.info_hash', '-e', 'bittorrent.msg.type',
@@ -110,7 +127,7 @@ def main(argv):
 
         print("Bootstrap nodes (sorted by port):")
         for node in bootstrap_nodes:
-            print(f"{node[0]}:{node[1]} {node[2]}")
+            print(f"{node[0]}:{node[1]} {node[2]} {node[3]}")
 
 
 if __name__ == "__main__":
