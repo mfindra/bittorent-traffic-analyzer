f7cda7f7b213817b8a4c07e75db18b3900d17d86
Michal Findra <miskofindra@gmail.com>
2023-04-18 22:18:21 +0200

    downloaded file parser

--- bt-monitor.py
+++ bt-monitor.py
@@ -2,6 +2,7 @@ import sys
 import csv
 import subprocess
 import tempfile
+from collections import defaultdict
 
 def extract_bootstrap_nodes(csv_file):
     bootstrap_nodes = set()
@@ -57,7 +58,7 @@ def extract_peers(csv_file):
 
                 nodes_flags = row[8]
                 peers_flags = row[9]
-                n_flags = nodes_flags.split(',')
+                n_flags = nodes_flags.split(',')                
                 if len(peers_flags) != 0:
                     peers_flags = peers_flags.split(',')
 
@@ -71,11 +72,6 @@ def extract_peers(csv_file):
                         peers_nodes.add(key)
                         connections[key] = 0
 
-                for i in range(len(peers_flags)):
-                    if peers_flags[i] == '1':
-                        key = (ips[i], int(ports[i]), ids[i])
-                        peers_nodes.add(key)
-                        connections[key] = 0
 
         # Second iteration to count connections
         csvfile.seek(0)  # Move the file pointer back to the beginning
@@ -93,6 +89,65 @@ def extract_peers(csv_file):
     return [(node[0], node[1], node[2], connections[node]) for node in sorted_peers_nodes]
 
 
+def extract_download(csv_file):
+    file_info = defaultdict(lambda: {"info_hash": "", "size": 0, "chunks": 0, "contributors": set()})
+    peers_nodes = set()
+
+    with open(csv_file, newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=';')
+        for row in csv_reader:
+            if row and 'nodes' in str(row) and str(row[7]).endswith("y,r"):
+                ip_field = row[5]
+                port_field = row[6]
+
+                nodes_flags = row[8]
+                peers_flags = row[9]
+                n_flags = nodes_flags.split(',')                
+                if len(peers_flags) != 0:
+                    peers_flags = peers_flags.split(',')
+
+                ips = (ip_field.split(','))
+                ports = (port_field.split(','))
+
+                for i in range(len(peers_flags)):
+                    if peers_flags[i] == '1':
+                        key = (ips[len(n_flags) +1 +i], int(ports[len(n_flags) +1 +i]))
+                        peers_nodes.add(key)
+
+        print(peers_nodes)
+
+        source = ""
+        info_hash = ""
+        csvfile.seek(0) 
+        for row in csv_reader:
+            if row[14] and row[15] and not row[16]:
+                print("handshake")
+                if row[2] in str(peers_nodes):
+                    source = row[2]
+                    info_hash = row[15]
+                    print("Downloading from peer")
+                    break
+        
+        csvfile.seek(0)
+        print(source)
+        pieces = []
+        contributors = 0
+        for row in csv_reader:
+            if row[1] == source and row[16] and "20" in str(row[16]) and "14" in str(row[16]):
+                contributors = 1
+                print(row[16])
+
+            if row[2] == source and row[13]:
+                len_pieces = row[13]
+                len_pieces = len_pieces.split(',')
+                for piece in len_pieces:
+                    pieces.append(piece)
+
+        pieces = [int(x, 16) for x in pieces]        
+
+        print(f"Length of file is: {sum(pieces)}, number of chunks: {len(pieces)}, info_hash is: {info_hash}, contributors: {contributors}")
+        
+    return
 
 
 
@@ -100,11 +155,27 @@ def process_pcap(pcap_file, mode):
     with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as temp_csv:
         tshark_command = [
             'tshark', '-r', pcap_file, '-T', 'fields', '-E', 'separator=;', '-d', 'udp.port==47222,bt-dht',
-            '-e', 'frame.time_relative', '-e', 'ip.src', '-e', 'ip.dst', '-e', 'udp.dstport', '-e', 'udp.srcport', '-e', 'bt-dht.ip', '-e', 'bt-dht.port',
-            '-e', 'bt-dht.bencoded.string', '-e', 'bt-dht.node', '-e', 'bt-dht.peer', '-e', 'bt-dht.id',
-            '-e', 'bittorrent.piece.index', '-e', 'bittorrent.piece.begin', '-e', 'bittorrent.piece.length',
-            '-e', 'bittorrent.peer_id', '-e', 'bittorrent.info_hash', '-e', 'bittorrent.msg.type',
-            '-e', 'tcp.srcport', '-e', 'tcp.dstport', '-e', 'dns.qry.name', '-e', 'dns.a', 'bt-dht or bittorrent or bt-utp or dns'
+            '-e', 'frame.time_relative',
+            '-e', 'ip.src',
+            '-e', 'ip.dst',
+            '-e', 'udp.dstport',
+            '-e', 'udp.srcport',
+            '-e', 'bt-dht.ip',
+            '-e', 'bt-dht.port',           
+            '-e', 'bt-dht.bencoded.string',
+            '-e', 'bt-dht.node',
+            '-e', 'bt-dht.peer',
+            '-e', 'bt-dht.id',           
+            '-e', 'bittorrent.piece.index',
+            '-e', 'bittorrent.piece.begin',
+            '-e', 'bittorrent.piece.length',           
+            '-e', 'bittorrent.peer_id',
+            '-e', 'bittorrent.info_hash',
+            '-e', 'bittorrent.msg.type',           
+            '-e', 'tcp.srcport',
+            '-e', 'tcp.dstport',
+            '-e', 'dns.qry.name',
+            '-e', 'dns.a', 'bt-dht or bittorrent or bt-utp or dns'
         ]
 
 
@@ -115,6 +186,8 @@ def process_pcap(pcap_file, mode):
                 return extract_bootstrap_nodes(temp_csv.name)
             elif mode == "peers":
                 return extract_peers(temp_csv.name)
+            elif mode == "download":
+                return extract_download(temp_csv.name)
         except subprocess.CalledProcessError as e:
             print(f"Error: TShark command failed with exit code {e.returncode}")
             return []
@@ -124,7 +197,7 @@ def main(argv):
     input_type = None
     init_mode = False
     peers_mode = False
-
+    download_mode = False 
 
     for i in range(len(argv)):
         if argv[i] == '-csv' or argv[i] == '-pcap':
@@ -134,6 +207,8 @@ def main(argv):
             init_mode = True
         elif argv[i] == '-peers':
             peers_mode = True
+        elif argv[i] == '-download':
+            download_mode = True
 
     if not input_path or not input_type:
         print("Error: No input path provided. Use -csv <path> or -pcap <path> to specify the path.")
@@ -159,6 +234,11 @@ def main(argv):
         for node in bootstrap_nodes:
             print(f"{node[0]}:{node[1]} {node[2]} {node[3]}")
 
+    if download_mode:
+        if input_type == '-csv':
+            extract_download(input_path)
+        elif input_type == '-pcap':
+            process_pcap(input_path, "download")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
