dadaa06eae3f9e7993a8661dcb5ad63bfecd8376
Michal Findra <miskofindra@gmail.com>
2023-05-25 22:59:52 +0200

    project finalization

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# PDS-project
+# Bittorrent pcap/csv file parser and analyzer
+
+## prerequisites: 
+1. tshark 4.0.7
+2. python 3.6.7<
+3. requirements.txt (pip install -r requirements.txt)
+
+## Usage
+The program accepts both pcap/pcapng and csv files. 
+Program is based on captured traffic from qBitTorrentClient.
+
+CSV file needs to follow strict form, which can be generated using following command
+``` bash
+tshark -r <filename> -T fields -E separator=";" -d udp.port==47222,bt-dht -e frame.time_relative -e ip.src -e ip.dst -e udp.dstport -e udp.srcport -e bt-dht.ip -e bt-dht.port -e bt-dht.bencoded.string -e bt-dht.node -e bt-dht.peer -e bt-dht.id -e bittorrent.piece.index  -e bittorrent.piece.length -e bittorrent.peer_id -e bittorrent.info_hash -e bittorrent.msg.type -e dns.a -e tcp.dstport "bt-dht or bittorrent or bt-utp or dns or udp or tcp"
+```
+
+PCAP/PCAPNG can just be supplied as argument and will be parsed internally. 
+
+### Syntax
+
+``` bash
+bt-monitor -pcap <file.pcap>|-csv <file.csv> -init | -peers | -download | -rtable
+```
+
+- `<file.pcap>`: Input PCAP file containing BitTorrent network traffic
+- `<file.csv>`: Input CSV file containing parsed BitTorrent network traffic
+
+### Functionalities
+
+- `-init`: Returns a list of detected bootstrap nodes (IP, port)
+- `-peers`: Returns a list of detected neighbors (IP, port, node ID, number of connections)
+- `-download`: Returns file information including info_hash, size, chunks, and contributing nodes (IP+port)
+- `-rtable`: Returns the routing table of the client (node IDs, IP, ports)
+
+### Usage Examples
+
+- Analyzing PCAP file:
+``` bash
+    ./bt-monitor -pcap test2.pcapng -init
+    ./bt-monitor -csv test2.csv -rtable
+```
--- bt-monitor.py
+++ bt-monitor.py
@@ -6,16 +6,16 @@ from enum import Enum
 
 # enum for types of peer messages
 class peerMessage(Enum):
-    CHOKE = "0"
-    UNCHOKE = "1"
-    INTERESTED = "2"
-    NOT_INTERESTED = "3"
-    HAVE = "4"
-    BITFIELD = "5"
-    REQUEST = "6"
-    PIECE = "7"
-    CANCEL = "8"
-    HAVE_ALL = "20"
+    CHOKE = '0'
+    UNCHOKE = '1'
+    INTERESTED = '2'
+    NOT_INTERESTED = '3'
+    HAVE = '4'
+    BITFIELD = '5'
+    REQUEST = '6'
+    PIECE = '7'
+    CANCEL = '8'
+    HAVE_ALL = '20'
 
 # enum for fields in csv file
 class csvFields(Enum):
@@ -36,6 +36,7 @@ class csvFields(Enum):
     BT_INFO_HASH = 14
     BT_MSG_TYPE = 15
     DNS_A = 16
+    TCP_DSTPORT = 17
 
 
 def extract_bootstrap_nodes(csv_file):
@@ -45,14 +46,14 @@ def extract_bootstrap_nodes(csv_file):
          # open file and prepare lines for parsing
         with open(csv_file, newline='') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=';')
-            # try parsing using "bs" key in bencoded data part
+            # try parsing using 'bs' key in bencoded data part
             for row in csv_reader:                                 
-                if row and 'get_peers' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,q") and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).startswith("a,bs,id"):
+                if row and 'get_peers' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,q') and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).startswith('a,bs,id'):
                     bootstrap_nodes.add((row[csvFields.IP_DST.value], row[csvFields.UDP_DSTPORT.value]))
                     
         # alternative method for getting bootstrap nodes, using DNS
         if len(bootstrap_nodes) == 0:
-            print("(DNS variant) ", end="")
+            print('(DNS variant) ', end='')
             all_nodes_set = set()
             dns_ips = set()
 
@@ -67,16 +68,16 @@ def extract_bootstrap_nodes(csv_file):
                             dns_ips.add(ip)
 
                     # save addresses, to which requests were made to
-                    if row and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,r"):                     
+                    if row and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,r'):                     
                         all_nodes_set.add((row[csvFields.IP_SRC.value],  int(row[csvFields.UDP_SRCPORT.value])))
                     
             bootstrap_nodes = {node for node in all_nodes_set if node[0] in dns_ips}
 
-        print("Bootstrap nodes:")
+        print('Bootstrap nodes:')
         for node in sorted(list(bootstrap_nodes), key=lambda x: x[1]):
-            print(f"{node[0]}:{node[1]}")
+            print(f'{node[0]}:{node[1]}')
     except (IOError, FileNotFoundError):
-        print(f"Error: Could not read file {csv_file}")
+        print(f'Error: Could not read file {csv_file}')
 
     return 
 
@@ -87,7 +88,8 @@ def extract_peers(csv_file):
         with open(csv_file, newline='') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=';')
             for row in csv_reader:
-                if row and 'nodes' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,r"):
+                # get_peers responses parser
+                if row and 'nodes' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,r'):
                     ip_field = row[csvFields.BT_DHT_IP.value]
                     port_field = row[csvFields.BT_DHT_PORT.value]
                     ids_field = row[csvFields.BT_DHT_ID.value]
@@ -114,14 +116,14 @@ def extract_peers(csv_file):
 
                     for i in range(len(peers_flags)):
                         if peers_flags[i] == '1':
-                            key = (ips[len(nodes_flags) +i], int(ports[len(nodes_flags) +i]), "none")
+                            key = (ips[len(nodes_flags) +i], int(ports[len(nodes_flags) +i]), 'none')
                             peers_nodes.add(key)
                             connections[key] = 0
             
             # Second iteration to count connections
             csvfile.seek(0)  
             for row in csv_reader:
-                if row and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,r"):
+                if row and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,r'):
                     src_ip = row[csvFields.IP_SRC.value]
                     src_port = int(row[csvFields.UDP_SRCPORT.value])
 
@@ -132,12 +134,12 @@ def extract_peers(csv_file):
         
         sorted_peers_nodes = sorted(list(peers_nodes), key=lambda x: x[1])
 
-        print("Neighbor nodes:")
+        print('Neighbor nodes:')
         for node in [(node[0], node[1], node[2], connections[node]) for node in sorted_peers_nodes]:
-            print(f"{node[0]}:{node[1]} {node[2]} {node[3]}")
+            print(f'{node[0]}:{node[1]} {node[2]} {node[3]}')
             
     except (IOError, FileNotFoundError):
-        print(f"Error: Could not read file {csv_file}")
+        print(f'Error: Could not read file {csv_file}')
     return
 
 def extract_download(csv_file):
@@ -146,7 +148,7 @@ def extract_download(csv_file):
         with open(csv_file, newline='') as csvfile:
             csv_reader = csv.reader(csvfile, delimiter=';')
 
-            info_hash = ""
+            info_hash = ''
             interested_peers = set()
             handshake_peers = set()
             
@@ -162,10 +164,18 @@ def extract_download(csv_file):
                     if row[csvFields.BT_PEER_ID.value] and row[csvFields.BT_INFO_HASH.value] and not row[csvFields.BT_MSG_TYPE.value] and (peer == row[csvFields.IP_DST.value]):
                         handshake_peers.add((peer, row[csvFields.BT_INFO_HASH.value]))
 
-            contributors = 0
             chunks = 0
             file_parts = []
+            contributors = set()
             
+            if len(interested_peers) == 0:
+                # try alternative method as interested packet, were omitted. 
+                csvfile.seek(0)
+                for row in csv_reader:    
+                    if row[csvFields.BT_PEER_ID.value] and row[csvFields.BT_INFO_HASH.value] and not row[csvFields.BT_MSG_TYPE.value]:                        
+                        handshake_peers.add((row[csvFields.IP_DST.value], row[csvFields.BT_INFO_HASH.value]))
+
+
             # extract chunks exchanged with Handshaked users by flag Request(6) in BitTorrent packets
             for peer in handshake_peers:
                 csvfile.seek(0)
@@ -174,13 +184,17 @@ def extract_download(csv_file):
 
                     sixes = 0
                     if row[csvFields.IP_DST.value] == peer[0] and row[csvFields.BT_PIECE_LENGTH.value]:
+                        if row[csvFields.TCP_DSTPORT.value]:
+                            contributors.add((row[csvFields.IP_DST.value], row[csvFields.TCP_DSTPORT.value]))
+                        elif row[csvFields.UDP_DSTPORT.value]:
+                            contributors.add((row[csvFields.IP_DST.value], row[csvFields.UDP_DSTPORT.value]))
                         len_pieces = row[csvFields.BT_PIECE_LENGTH.value].split(',')
                         ids_list = row[csvFields.BT_PIECE_INDEX.value].split(',')
                         flags = row[csvFields.BT_MSG_TYPE.value].split(',')
 
 
                         for i in range(len(ids_list)):
-                            if flags[i] == "6":
+                            if flags[i] == '6':
                                 ids.append((peer[0], ids_list[i], int(len_pieces[sixes], 16)))
                                 sixes+=1
 
@@ -200,8 +214,6 @@ def extract_download(csv_file):
             # extract info_hash and number of contributors based on amount of chunk exchanged.
             max_chunks = -1
             for i, part in enumerate(file_parts):
-                if len(part) > 0:
-                    contributors+=1
                 if max_chunks < len(part):
                     max_chunks = len(part)
                     info_hash = list(handshake_peers)[i][1]
@@ -217,53 +229,13 @@ def extract_download(csv_file):
                     chunks+=1
 
 
-            print(f"Length of file is: {sum(max_values.values())}, number of parts: {chunks}, info_hash is: {info_hash}, contributors: {contributors}")
+            print(f'Length of file is: {sum(max_values.values())}, number of parts: {chunks}, info_hash is: {info_hash}, contributors: {contributors}')
 
     except (IOError, FileNotFoundError):
-        print(f"Error: Could not read file {csv_file}")
+        print(f'Error: Could not read file {csv_file}')
 
     return
 
-
-# parse pcpa file from input to desired csv file
-def process_pcap(pcap_file, mode):
-    with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as temp_csv:
-        tshark_command = [
-            'tshark', '-r', pcap_file, '-T', 'fields', '-E', 'separator=;', '-d', 'udp.port==47222,bt-dht',
-            '-e', 'frame.time_relative',
-            '-e', 'ip.src',
-            '-e', 'ip.dst',
-            '-e', 'udp.dstport',
-            '-e', 'udp.srcport',
-            '-e', 'bt-dht.ip',
-            '-e', 'bt-dht.port',           
-            '-e', 'bt-dht.bencoded.string',
-            '-e', 'bt-dht.node',
-            '-e', 'bt-dht.peer',
-            '-e', 'bt-dht.id',           
-            '-e', 'bittorrent.piece.index',
-            '-e', 'bittorrent.piece.length',           
-            '-e', 'bittorrent.peer_id',
-            '-e', 'bittorrent.info_hash',
-            '-e', 'bittorrent.msg.type',           
-            '-e', 'dns.a', 'bt-dht or bittorrent or bt-utp or dns'
-        ]
-
-
-        try:
-            subprocess.run(tshark_command, stdout=temp_csv, check=True)
-            temp_csv.seek(0)
-            if mode == "init":
-                return extract_bootstrap_nodes(temp_csv.name)
-            elif mode == "peers":
-                return extract_peers(temp_csv.name)
-            elif mode == "download":
-                return extract_download(temp_csv.name)
-            elif mode == "rtable":
-                return create_routing_table(temp_csv.name)
-        except subprocess.CalledProcessError as e:
-            print(f"Error: TShark command failed with exit code {e.returncode}")
-            return
 
 def create_routing_table(csv_file):
     peers_nodes = set()
@@ -283,21 +255,19 @@ def create_routing_table(csv_file):
                 for ip in dnss_split:
                     dns_ips.add(ip)
 
-        
-
         csvfile.seek(0)  
         for row in csv_reader:
             
             # extract my id(s)
             if (row and not 'announce_peer' in str(row) and 
                 not 'find_node' in str(row) and 
-                str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,q") and 
-                str(row[csvFields.BT_DHT_BENCODED_STRING.value]).startswith("a,id") and
+                str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,q') and 
+                str(row[csvFields.BT_DHT_BENCODED_STRING.value]).startswith('a,id') and
                 str(row[csvFields.IP_SRC.value]) in str(my_ips)):
                 my_id.add(row[csvFields.BT_DHT_BENCODED_STRING.value].split(',')[2])
 
             # extract get_peers responses
-            if row and 'nodes' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith("y,r"):
+            if row and 'nodes' in str(row) and str(row[csvFields.BT_DHT_BENCODED_STRING.value]).endswith('y,r'):
                     ip_field = row[csvFields.BT_DHT_IP.value]
                     port_field = row[csvFields.BT_DHT_PORT.value]
                     ids_field = row[csvFields.BT_DHT_ID.value]
@@ -321,10 +291,6 @@ def create_routing_table(csv_file):
                             key = (ips[i], int(ports[i]), ids[i])
                             peers_nodes.add(key)                        
 
-
-
-    #print(my_id)
-    #print(peers_nodes)
     
     def xor_and_count_leading_zeros(num1, num2):
         # Determine the maximum length of the input hexadecimal numbers
@@ -346,24 +312,60 @@ def create_routing_table(csv_file):
                 break
 
         return count
-
         
     distance_table = set()
-    peer_info = set()
-
-      
 
     for my_id_value in my_id:
         for ip, port, node_id in peers_nodes:
             distance_table.add((my_id_value, xor_and_count_leading_zeros(str(my_id_value), str(node_id)), ip, port, node_id))
 
     # Print the distance table as a formatted table
-    print("{:<40} {:<10} {:<15} {:<8} {}".format("my id", "distance", "ip", "port", "node_id"))
-    print("---------------------------------------------------------------------------------------------------------------------")
+    print('{:<40} {:<10} {:<15} {:<8} {}'.format('my id', 'distance', 'ip', 'port', 'node_id'))
+    print('---------------------------------------------------------------------------------------------------------------------')
     for my_id, distance, ip, port, node_id in distance_table:
-        print("{:<40} {:<10} {:<15} {:<8} {}".format(my_id, distance, ip, port, node_id))
+        print('{:<40} {:<10} {:<15} {:<8} {}'.format(my_id, distance, ip, port, node_id))
 
+# parse pcap file from input to desired csv file
+def process_pcap(pcap_file, mode):
+    with tempfile.NamedTemporaryFile(mode='w+t', delete=False) as temp_csv:
+        tshark_command = [
+            'tshark', '-r', pcap_file, '-T', 'fields', '-E', 'separator=;', '-d', 'udp.port==47222,bt-dht',
+            '-e', 'frame.time_relative',
+            '-e', 'ip.src',
+            '-e', 'ip.dst',
+            '-e', 'udp.dstport',
+            '-e', 'udp.srcport',
+            '-e', 'bt-dht.ip',
+            '-e', 'bt-dht.port',           
+            '-e', 'bt-dht.bencoded.string',
+            '-e', 'bt-dht.node',
+            '-e', 'bt-dht.peer',
+            '-e', 'bt-dht.id',           
+            '-e', 'bittorrent.piece.index',
+            '-e', 'bittorrent.piece.length',           
+            '-e', 'bittorrent.peer_id',
+            '-e', 'bittorrent.info_hash',
+            '-e', 'bittorrent.msg.type',           
+            '-e', 'dns.a',
+            '-e', 'tcp.dstport',
+            'bt-dht or bittorrent or bt-utp or dns or tcp'
+        ]
 
+        # execute functionality on parsed pcap file
+        try:
+            subprocess.run(tshark_command, stdout=temp_csv, check=True)
+            temp_csv.seek(0)
+            if mode == 'init':
+                return extract_bootstrap_nodes(temp_csv.name)
+            elif mode == 'peers':
+                return extract_peers(temp_csv.name)
+            elif mode == 'download':
+                return extract_download(temp_csv.name)
+            elif mode == 'rtable':
+                return create_routing_table(temp_csv.name)
+        except subprocess.CalledProcessError as e:
+            print(f'Error: TShark command failed with exit code {e.returncode}')
+            return
 
 def main(argv):
     input_path = None
@@ -387,37 +389,37 @@ def main(argv):
         elif argv[i] == '-rtable':
             rtable_mode = True
         elif argv[i] != input_path:
-            print("Error: Unrecognized argument " + argv[i] + ".")
+            print('Error: Unrecognized argument ' + argv[i] + '.')
             return
 
     if not input_path or not input_type:
-        print("Error: No input path provided. Use -csv <path> or -pcap <path> to specify the path.")
+        print('Error: No input path provided. Use -csv <path> or -pcap <path> to specify the path.')
         return
 
     if init_mode:
         if input_type == '-csv':
             extract_bootstrap_nodes(input_path)
         elif input_type == '-pcap':
-            process_pcap(input_path, "init")
+            process_pcap(input_path, 'init')
 
     if peers_mode:
         if input_type == '-csv':
             extract_peers(input_path)
         elif input_type == '-pcap':
-            process_pcap(input_path, "peers")
+            process_pcap(input_path, 'peers')
 
     if download_mode:
         if input_type == '-csv':
             extract_download(input_path)
         elif input_type == '-pcap':
-            process_pcap(input_path, "download")
+            process_pcap(input_path, 'download')
 
     if rtable_mode:
         if input_type == '-csv':
             create_routing_table(input_path)
         elif input_type == '-pcap':
-            process_pcap(input_path, "rtable")
+            process_pcap(input_path, 'rtable')
             
         
-if __name__ == "__main__":
+if __name__ == '__main__':
     main(sys.argv[1:])
